a28d6bc6c66b3a733488cfc296089973f9453887
爬山過海肥肥豬 <smartbean@KC-MacBook-Pro.local>
2023-02-12 15:51:17 -0500

    improve css and add tabNav

---
+++ pages/components/TabNav.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import styles from '../../styles/tab.module.css'
+import TabItem from '../Tabitem'
+
+const TabList = [
+  {
+    text: 'Everyday Health',
+    href: '/everyday-health',
+  },
+  {
+    text: 'All',
+    href: '/blog',
+  },
+  {
+    text: 'Hair',
+    href: '/hair',
+  },
+  {
+    text: 'Lifestyle',
+    href: '/lifestyle',
+  },
+  {
+    text: 'Mental Health',
+    href: '/mental-health',
+  },
+  {
+    text: 'Sex',
+    href: '/sex',
+  },
+  {
+    text: 'Skin',
+    href: '/skin',
+  },
+]
+
+function TabNav() {
+  const [navActive, setNavActive] = useState<Boolean>()
+  const [activeIdx, setActiveIdx] = useState(1)
+
+  return (
+    <div className={styles.tabNavContainer}>
+      {TabList.map((tab, idx) => (
+        <div
+          onClick={() => {
+            setActiveIdx(idx)
+            setNavActive(true)
+          }}
+          key={tab.text}
+          className={styles.tabitem}
+        >
+          <TabItem active={activeIdx === idx} {...tab} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default TabNav
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,29 @@ import All from './All'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
+import { useEffect, useState } from 'react'
+import TabNav from './components/TabNav'
+
 export default function Home() {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    function handleScroll() {
+      console.log(window.scrollY)
+      if (window.scrollY > 760) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  })
+
   return (
     <div>
-      <Navbar />
+      {scrolled ? <TabNav /> : <Navbar />}
+
       <App Component={All} />
       <Footer />
     </div>
